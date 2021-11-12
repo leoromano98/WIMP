2d7022566ae2123e95940d350aee62aef685298a
leoromano98 <59894455+leoromano98@users.noreply.github.com>
2021-11-12 10:12:43 -0300

    remove build

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,3 @@
-export const API_HOST = "https://refactor-wimp.herokuapp.com";
+// export const API_HOST = "https://refactor-wimp.herokuapp.com";
+export const API_HOST = "http://localhost:49160/";
 export const TOKEN = "token";
