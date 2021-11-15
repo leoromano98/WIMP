ce13fe08024d44b73ea66eda0b3c6fa2a7ab4529
leoromano98 <59894455+leoromano98@users.noreply.github.com>
2021-11-15 01:26:23 -0300

    go to login after lgout

--- src/components/BackHome/BackHome.js
+++ src/components/BackHome/BackHome.js
@@ -3,9 +3,14 @@ import { Button } from "reactstrap";
 import { logoutApi } from "../../api/auth";
 
 const BackHome = () => {
+  const handleLogoutClick = () => {
+    logoutApi();
+    window.location.reload();
+  };
+
   return (
     <div class="back-container">
-      <Button type="button" onClick={logoutApi}>
+      <Button type="button" onClick={handleLogoutClick}>
         Cerrar sesión y salir
       </Button>
     </div>
