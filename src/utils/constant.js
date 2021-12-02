f07122ae4fd36ceeff36cd057f887b72204a5901
leoromano98 <59894455+leoromano98@users.noreply.github.com>
2021-12-01 21:51:07 -0300

    add env var

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const API_HOST = "https://refactor-wimp.herokuapp.com";
+export const API_HOST = process.env.REACT_APP_API_HOST ? process.env.REACT_APP_API_HOST : "https://refactor-wimp.herokuapp.com";
 export const TOKEN = "token";
